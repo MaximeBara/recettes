2c03e72ba45e0b73f648efb5ea99b859f7d780c1
Maxime Baranowski <emouvaxy@gmail.com>
2021-01-29 10:36:33 +0100

    Initialisation du projet

---
+++ src/test/java/fr/m2i/recettes/RecettesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.recettes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecettesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
